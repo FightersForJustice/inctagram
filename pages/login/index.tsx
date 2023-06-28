529ab8a352b1b5577fc576cc056207b0284846ee
TURP1 <turpicrypto@gmail.com>
2023-06-28 03:50:35 +0300

    feat: add page /login

---
+++ pages/login/index.tsx
@@ -0,0 +1,24 @@
+import { PageWrapper } from "components/PageWrapper/PageWrapper";
+import LoginForm from "@/components/Login/LoginForm";
+import { getLayout } from "@/components/Layout/Layout";
+
+// Next запускает эту функцию, и потом рендерит страничку (в зависимости  от пропсов , которые придут). Вызывается единожды на сервере в момент билда, после страничка кешируется
+export const getStaticProps = async () => {
+
+    return {
+        props: {}
+    }
+}
+
+
+const Login = () => {
+    
+    return (
+        <PageWrapper>
+          <LoginForm></LoginForm>
+        </PageWrapper>
+    );
+}
+
+Login.getLayout = getLayout
+export default Login;

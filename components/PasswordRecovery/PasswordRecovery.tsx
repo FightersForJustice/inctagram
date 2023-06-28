8de6cd691a0658ce06aefa8ecd9e87c23f1186cf
Qurial <konvert955@gmail.com>
2023-06-29 01:15:04 +0300

    style: add styles to password recovery component

--- components/PasswordRecovery/PasswordRecovery.tsx
+++ components/PasswordRecovery/PasswordRecovery.tsx
@@ -1,5 +1,7 @@
 import { ChangeEvent, useState, FC, PropsWithChildren } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import style from '../Login/LoginForm.module.css'
+import StyledInput, { StyledInputPassword } from './StyledInput'
 
 interface IFormInput {
   password: string
@@ -14,7 +16,6 @@ const PasswordRecovery: FC<PropsWithChildren<{}>> = ({ children }) => {
   } = useForm<IFormInput>()
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const [isPasswordShown, setIsPasswordShown] = useState({ password: false, confirmPassword: false })
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     alert('new password set')
     setPassword('')
@@ -24,54 +25,52 @@ const PasswordRecovery: FC<PropsWithChildren<{}>> = ({ children }) => {
     setValue(e.target.value)
   }
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register('password', {
-          required: true,
-          maxLength: 20,
-          minLength: 6,
-          pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
-          onChange: (e) => handleChange(e, setPassword),
-          validate: (v) => v === confirmPassword,
-        })}
-        type={isPasswordShown.password ? 'text' : 'password'}
-        id="password"
-      />
-      <label htmlFor="password" onClick={() => setIsPasswordShown((prev) => ({ ...prev, password: !prev.password }))}>
-        o
-      </label>
-      <br></br>
+    <div className={style.mainContainer} style={{ fontFamily: 'inter, sans-serif' }}>
+      <div className={style.form_wrapper}>
+        <h1 style={{ color: 'white', fontWeight: '700', fontSize: '20px' }}>Create New Password</h1>
+        <form className={style.FormRoot} style={{ marginTop: '40px' }} onSubmit={handleSubmit(onSubmit)}>
+          <StyledInputPassword
+            validation={{
+              ...register('password', {
+                required: true,
+                maxLength: 20,
+                minLength: 6,
+                pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+                onChange: (e) => handleChange(e, setPassword),
+                validate: (v) => v === confirmPassword,
+              })
+            }}
+            id="password"
+            label={'New password'}
+          />
+          <StyledInputPassword
+            validation={{...register('confirmPassword', {
+                required: true,
+                maxLength: 20,
+                minLength: 6,
+                pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+                onChange: (e) => handleChange(e, setConfirmPassword),
+                validate: (v) => v === password,
+              })
+            }}
+            id="confirmPassword"
+            label={'Password confirmation'}
+          />
+          <div style={{ color: '#8d9094' }}>
+            {errors.confirmPassword && (errors.confirmPassword.type === 'minLength' || errors.confirmPassword.type === 'maxLength') && (
+              <p>Your password must be between 6 and 20 characters</p>
+            )}
+            {errors.confirmPassword && errors.password && errors.confirmPassword.type === 'required' && <p>Password field is empty</p>}
+            {errors.confirmPassword && errors.password && errors.confirmPassword.type === 'validate' && <p>Passwords doesn't match</p>}
+            {errors.confirmPassword && errors.confirmPassword.type === 'pattern' && (
+              <p>Your password must contain at least one number and one character</p>
+            )}
+            <input type="submit" style={{ fontSize: '16px', fontWeight: '600', marginTop: '20px' }} className={style.Button} value='Create New Password' />
 
-      <input
-        {...register('confirmPassword', {
-          required: true,
-          maxLength: 20,
-          minLength: 6,
-          pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
-          onChange: (e) => handleChange(e, setConfirmPassword),
-          validate: (v) => v === password,
-        })}
-        type={isPasswordShown.confirmPassword ? 'text' : 'password'}
-        id="confirmPassword"
-      />
-      <label
-        htmlFor="confirmPassword"
-        onClick={() => setIsPasswordShown((prev) => ({ ...prev, confirmPassword: !prev.confirmPassword }))}
-      >
-        o
-      </label>
-      <br></br>
-
-      {errors.confirmPassword && (errors.confirmPassword.type === 'minLength' || errors.confirmPassword.type === 'maxLength') && (
-        <p>Your password must be between 6 and 20 characters</p>
-      )}
-      {errors.confirmPassword && errors.password && errors.confirmPassword.type === 'required' && <p>Password field is empty</p>}
-      {errors.confirmPassword && errors.password && errors.confirmPassword.type === 'validate' && <p>Passwords doesn't match</p>}
-      {errors.confirmPassword && errors.confirmPassword.type === 'pattern' && (
-        <p>Your password must contain at least one number and one character</p>
-      )}
-      <input type="submit" />
-    </form>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 

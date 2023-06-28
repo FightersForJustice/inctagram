fcfc0da25e9120eba3d2323fd53b64a7c5649085
Qurial <konvert955@gmail.com>
2023-06-29 01:13:40 +0300

    refactor: add own component for input field

---
+++ components/PasswordRecovery/StyledInput.tsx
@@ -0,0 +1,37 @@
+import style from '@/components/Login/LoginForm.module.css'
+import { useState } from 'react'
+
+interface IStyledInputProps extends React.HTMLAttributes<HTMLInputElement> {
+  validation?: object
+  type?: string
+  label?: string
+}
+
+const StyledInput: React.FC<IStyledInputProps> = ({ validation, ...props }) => {
+  return <div className={style.FormField} style={{marginTop: '20px'}} >
+    {props.label && <label style={{color: '#8d9094', position: 'absolute', top: '0px'}}>{props.label}</label>}
+    <input className={style.Input}  {...props} {...validation} />
+  </div>
+}
+
+export const StyledInputPassword: React.FC<IStyledInputProps> = ({ ...props }) => {
+  const [isPasswordShown, setIsPasswordShown] = useState(false)
+  return <div style={{ position: 'relative' }}>
+    <StyledInput {...props} type={isPasswordShown ? 'text' : 'password'} />
+    <span
+      style={
+        {
+          position: 'absolute',
+          right: '10px',
+          top: '20px',
+          fontSize: '22px',
+          color: 'white',
+          cursor: 'pointer'
+        }}
+      onClick={() => setIsPasswordShown(prev => !prev)}
+    >{isPasswordShown ? <span>&#8413;</span> : <span>&#8416;</span>}</span>
+  </div>
+
+}
+
+export default StyledInput

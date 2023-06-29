0ee5cbca0e103944fb23993c05ba9a97c3b0f969
Iryna Rudzko <irinrudko@gmail.com>
2023-06-29 14:44:32 +0200

    refactor: change statusCode

--- assets/api/auth/authTypes.ts
+++ assets/api/auth/authTypes.ts
@@ -17,7 +17,7 @@ export type ServerSuccessResponse = {
   accessToken: string
 }
 export type ServerErrorResponse = {
-  statusCode: number
+  statusCode: 400 | 500
   messages: ResponseMessage[]
   error: string
 }

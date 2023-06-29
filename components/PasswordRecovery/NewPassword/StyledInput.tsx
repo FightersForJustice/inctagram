12e0ec4d2d60100f75885c5772aed8f528835030
Qurial <konvert955@gmail.com>
2023-06-29 14:03:21 +0300

    refactor: styled input component

--- components/PasswordRecovery/NewPassword/StyledInput.tsx
+++ components/PasswordRecovery/NewPassword/StyledInput.tsx
@@ -8,15 +8,17 @@ interface IStyledInputProps extends React.HTMLAttributes<HTMLInputElement> {
 }
 
 const StyledInput: React.FC<IStyledInputProps> = ({ validation, ...props }) => {
-  return <div className={style.FormField} style={{marginTop: '20px'}} >
-    {props.label && <label style={{color: '#8d9094', position: 'absolute', top: '0px'}}>{props.label}</label>}
-    <input className={style.Input}  {...props} {...validation} />
+  return <div style={{ position: 'relative' }}>
+    <div className={style.FormField} style={{ marginTop: '20px' }} >
+      {props.label && <label style={{ color: '#8d9094', position: 'absolute', top: '0px' }}>{props.label}</label>}
+      <input className={style.Input}  {...props} {...validation} />
+    </div>
   </div>
 }
 
 export const StyledInputPassword: React.FC<IStyledInputProps> = ({ ...props }) => {
   const [isPasswordShown, setIsPasswordShown] = useState(false)
-  return <div style={{ position: 'relative' }}>
+  return <>
     <StyledInput {...props} type={isPasswordShown ? 'text' : 'password'} />
     <span
       style={
@@ -30,7 +32,7 @@ export const StyledInputPassword: React.FC<IStyledInputProps> = ({ ...props }) =
         }}
       onClick={() => setIsPasswordShown(prev => !prev)}
     >{isPasswordShown ? <span>&#8413;</span> : <span>&#8416;</span>}</span>
-  </div>
+  </>
 
 }
 

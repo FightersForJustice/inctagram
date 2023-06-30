03e669d69952dacf0e333f3e14581396bcbc2f56
ilich01 <sidea.ilyaderkach@gmail.com>
2023-06-30 15:19:35 +0300

    refactor: header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -12,16 +12,11 @@ interface Props {
   outlineBellMask: string
 }
 
-export const Header = ({
-  className,
-  languageLanguageClassName,
-  languageFlagRussiaFlagRussiaClassName,
-  outlineBellMask = 'image.svg',
-}: Props): JSX.Element => {
+export const Header = (): JSX.Element => {
   return (
     <div className={s.header}>
       <Logo className={s.inctagram} />
-      <OutlineBell className={s.outline_bell_instance} mask={outlineBellMask} />
+      <OutlineBell className={s.outline_bell_instance} mask={'image.svg'} />
       <Language className={s.languageLanguage} />
     </div>
   )

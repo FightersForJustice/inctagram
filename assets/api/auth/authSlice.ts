cb35e9dc69d9cd50d9ad2e03bcff55065dcbc4d1
TURP1 <turpicrypto@gmail.com>
2023-07-06 14:32:09 +0300

    feat: add validation on spaces

--- assets/api/auth/authSlice.ts
+++ assets/api/auth/authSlice.ts
@@ -1,29 +1,25 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface User {
-  userId: number;
-  userName: string;
-  email: string;
+  userId: number
+  userName: string
+  email: string
 }
 
-interface AuthState {
-  user: User | null;
+const initialState = {
+  user: null as User | null,
 }
-
-const initialState: AuthState = {
-  user: null,
-};
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     setUser: (state, action: PayloadAction<User>) => {
-      state.user = action.payload;
+      state.user = action.payload
     },
   },
-});
+})
 
-export const { setUser } = authSlice.actions;
+export const { setUser } = authSlice.actions
 
-export default authSlice.reducer;
+export default authSlice.reducer

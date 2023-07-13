03dfdf53a3b4112ef9e42439d188afdd75832fb3
Iryna Rudzko <irinrudko@gmail.com>
2023-07-13 17:33:39 +0200

    feat: add userRouts

---
+++ components/common/User/userRouts.ts
@@ -0,0 +1,8 @@
+export const userRouts = {
+  home: '/home',
+  profileSettings: '/home/settings',
+  generalInfo: '/home/settings/general',
+  devices: '/home/settings/devices',
+  account: '/home/settings/account',
+  payments: '/home/settings/payments',
+}

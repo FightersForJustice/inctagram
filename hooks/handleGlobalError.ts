1573eac0c7761b2f0ff8644483bf72e4fb50e043
Iryna Rudzko <irinrudko@gmail.com>
2023-07-13 11:28:20 +0200

    fix: app crashes when offline

--- hooks/handleGlobalError.ts
+++ hooks/handleGlobalError.ts
@@ -4,13 +4,13 @@ import { Dispatch, MiddlewareAPI, PayloadAction, isRejectedWithValue } from '@re
 export const handleGlobalError = (api: MiddlewareAPI) => (next: Dispatch) => (action: PayloadAction<ErrorPayloadType>) => {
   if (isRejectedWithValue(action)) {
     const status = action.payload.status
-    const error: any = action.payload.data?.messages[0].message || action.payload.data.messages
 
     if (status === 500 || status === 504) {
       toast.error('Oops! Try again later') //fix
     }
 
     try {
+      const error: any = action.payload.data?.messages[0].message || action.payload.data.messages
       error && toast.error(error)
     } catch {
       toast.error('Sorry, something went wrong')

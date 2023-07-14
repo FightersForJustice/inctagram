bb12c172e5eba31130f2a1881351147f19599b5e
Qurial <konvert955@gmail.com>
2023-07-14 21:53:03 +0300

    refactor: ui-kit-icons

--- pages/dev/ui-kit-icons/index.tsx
+++ pages/dev/ui-kit-icons/index.tsx
@@ -1,6 +1,7 @@
 import { PageWrapper } from 'components/PageWrapper/PageWrapper'
 import { getLayout } from '@/components/Layout/Layout'
 import { Icons } from '@/ui/ui-kit/Icon/IconsComponent'
+import style from '@/ui/ui-kit/Icon/IconsComponent.module.scss'
 
 export const getStaticProps = async () => {
   return {
@@ -11,13 +12,38 @@ export const getStaticProps = async () => {
 const Failed = () => {
   return (
     <PageWrapper>
-      <Icons.Home isActive/>
-      <Icons.Create />
-      <Icons.Profile />
-      <Icons.Messenger />
-      <Icons.Search />
-      <Icons.Statistics />
-      <Icons.Logout isDisabled />
+      <div style={{ display: 'flex', textAlign: 'center', color: 'white' }}>
+        <div>
+          <h2>Default</h2>
+          <Icons.Home className={style.SideBar__Item} />
+          <Icons.Create className={style.SideBar__Item} />
+          <Icons.Profile className={style.SideBar__Item} />
+          <Icons.Messenger className={style.SideBar__Item} />
+          <Icons.Search className={style.SideBar__Item} />
+          <Icons.Statistics className={style.SideBar__Item} />
+          <Icons.Logout className={style.SideBar__Item} />
+        </div>
+        <div>
+          <h2>Active</h2>
+          <Icons.Home className={style.SideBar__Item} isActive />
+          <Icons.Create className={style.SideBar__Item} />
+          <Icons.Profile className={style.SideBar__Item} />
+          <Icons.Messenger className={style.SideBar__Item} />
+          <Icons.Search className={style.SideBar__Item} />
+          <Icons.Statistics className={style.SideBar__Item} />
+          <Icons.Logout className={style.SideBar__Item} />
+        </div>
+        <div>
+          <h2>Disabled</h2>
+          <Icons.Home className={style.SideBar__Item} isDisabled />
+          <Icons.Create className={style.SideBar__Item} />
+          <Icons.Profile className={style.SideBar__Item} />
+          <Icons.Messenger className={style.SideBar__Item} />
+          <Icons.Search className={style.SideBar__Item} />
+          <Icons.Statistics className={style.SideBar__Item} />
+          <Icons.Logout className={style.SideBar__Item} />
+        </div>
+      </div>
     </PageWrapper>
   )
 }

e34840df29c7ba0e48c9d1a903dfb54fdfd5ab08
Iryna Rudzko <irinrudko@gmail.com>
2023-07-17 00:34:39 +0200

    feat: add profile-settings button

---
+++ pages/home/index.tsx
@@ -0,0 +1,29 @@
+import { getLayout } from '@/components/Layout/Layout'
+import { PageWrapper } from '@/components/PageWrapper/PageWrapper'
+import { userRouts } from '@/components/common/User/userRouts'
+import { useRouter } from 'next/router'
+
+const Home = () => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(userRouts.profileSettings)
+  }
+
+  return (
+    <PageWrapper>
+      <button
+        onClick={handleClick}
+        style={{
+          width: '200px',
+          height: '50px',
+        }}
+      >
+        Profile Settings
+      </button>
+    </PageWrapper>
+  )
+}
+
+Home.getLayout = getLayout
+export default Home

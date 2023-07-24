7658be7b2a56b53eae4398aa8b639837c3a38ea2
Qurial <konvert955@gmail.com>
2023-07-25 00:04:47 +0300

    dev: add saving value to state

--- @ui/ui-kit/DatePicker/DatePicker.tsx
+++ @ui/ui-kit/DatePicker/DatePicker.tsx
@@ -5,14 +5,20 @@ import './DatePicker.scss'
 import 'dayjs/locale/ru'
 import 'dayjs/locale/en'
 import { useTranslation } from 'react-i18next'
+import { useState } from 'react'
 
 export const MainDatePicker = () => {
   const { t } = useTranslation()
-
+  const [date, setDate] = useState()
   return (
     <div className="UIKitDatePicker">
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={t('locale')}>
-        <DatePicker open showDaysOutsideCurrentMonth dayOfWeekFormatter={(day) => day} />
+        <DatePicker
+          showDaysOutsideCurrentMonth
+          dayOfWeekFormatter={(day) => day}
+          onChange={(e: any) => setDate(e.$d)}
+          value={date}
+        />
       </LocalizationProvider>
     </div>
   )

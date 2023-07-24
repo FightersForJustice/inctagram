f54b4bace357acc8a8599e8f072b92ae8eb933e9
Qurial <konvert955@gmail.com>
2023-07-24 17:56:44 +0300

    feat: add translation

--- @ui/ui-kit/DatePicker/DatePicker.tsx
+++ @ui/ui-kit/DatePicker/DatePicker.tsx
@@ -2,11 +2,16 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import './DatePicker.scss'
+import 'dayjs/locale/ru'
+import 'dayjs/locale/en'
+import { useTranslation } from 'react-i18next'
 
 export const MainDatePicker = () => {
+  const { t } = useTranslation()
+
   return (
     <div className="UIKitDatePicker">
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={t('locale')}>
         <DatePicker open showDaysOutsideCurrentMonth dayOfWeekFormatter={(day) => day} />
       </LocalizationProvider>
     </div>

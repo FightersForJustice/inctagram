3d7e66403c92b7dbd858a229a3b5129cc96aeaa7
Iryna Rudzko <irinrudko@gmail.com>
2023-07-26 10:49:16 +0200

    feat: add tocken

--- assets/api/instance.ts
+++ assets/api/instance.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+const accessToken = localStorage.getItem('accessToken')
+
 export const instance = axios.create({
   baseURL: 'https://inctagram-api.vercel.app/api',
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
 })

d6c8c2f81a67f2f8af955a15c5fa08951d98b825
Iryna Rudzko <irinrudko@gmail.com>
2023-07-26 23:26:17 +0200

    refactor: types

--- assets/api/user/profileQueryApi.ts
+++ assets/api/user/profileQueryApi.ts
@@ -1,8 +1,8 @@
 import { baseUrl } from '@/assets/api/common.api'
 import { userRouts } from '@/components/common/User/userRouts'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { userProfile, UpdateuserProfile } from './userTypes'
 import { ServerErrorResponse } from '../auth/authTypes'
+import { UpdateUserProfile, UserProfile } from './userTypes'
 
 export const profileQueryApi = createApi({
   reducerPath: 'profileApi',
@@ -11,7 +11,7 @@ export const profileQueryApi = createApi({
     credentials: 'include',
   }),
   endpoints: (builder) => ({
-    getProfile: builder.query<userProfile, void>({
+    getProfile: builder.query<UserProfile, void>({
       query: () => ({
         url: userRouts.profile,
         method: 'GET',
@@ -20,7 +20,7 @@ export const profileQueryApi = createApi({
         },
       }),
     }),
-    updateProfile: builder.mutation<void | ServerErrorResponse, UpdateuserProfile>({
+    updateProfile: builder.mutation<void | ServerErrorResponse, UpdateUserProfile>({
       query: (userData) => ({
         url: userRouts.profile,
         method: 'PUT',

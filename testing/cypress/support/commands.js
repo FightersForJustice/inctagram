2edb256f5590bd50b23e6b32bc92a2a6e49fb729
nataliasiutsova <tashaklimenkova@gmail.com>
2023-07-27 10:47:21 +0300

    Update Login

--- testing/cypress/support/commands.js
+++ testing/cypress/support/commands.js
@@ -1,26 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import { faker } from "@faker-js/faker"
+const regSelectors = require('../fixtures/registrationSelectors.json')
+
+// Cypress.Commands.add('generateUser', () => {
+//   const { faker } = require('@faker-js/faker')
+//   cy.writeFile('cypress/fixtures/user.json', {
+//     userName: faker.person.fullName(),
+//     userEmail: faker.internet.email(),
+//     userPassword: faker.internet.password({ length: 6 }),
+//   })
+// })
+
+Cypress.Commands.add('registration', (userName, userEmail, userPassword) => {
+  cy.get(regSelectors.Name).type(userName)
+  cy.get(regSelectors.Mail).type(userEmail)
+  cy.get(regSelectors.Password).type(userPassword)
+  cy.get(regSelectors.Password2).type(userPassword)
+  cy.get(regSelectors.ButtonSignUp).click()
+})

6853f8db177cf1d7141b4ca093e863314e650ca3
Iryna Rudzko <irinrudko@gmail.com>
2023-07-29 18:28:46 +0200

    refactor: remove not used code

--- pages/home/profile-settings/index.tsx
+++ pages/home/profile-settings/index.tsx
@@ -2,20 +2,11 @@ import { serverAPI } from '@/assets/api/api'
 import { UserProfile } from '@/assets/api/user/userTypes'
 import { getLayout } from '@/components/Layout/Layout'
 import { PageWrapper } from '@/components/PageWrapper/PageWrapper'
-import { authRouts } from '@/components/common/Auth/authRoutes'
 import ProfileTabs from '@/components/profile/Tabs/ProfileTabs'
-import { checkAuth } from '@/utils/checkAuth'
 import { GetServerSideProps, NextApiRequest } from 'next'
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  if (!checkAuth()) {
-    return {
-      redirect: {
-        destination: authRouts.notAuthorized,
-        permanent: true,
-      },
-    }
-  }
+  //check isAuth
 
   const userProfile = await serverAPI.profile.getProfileFromServer(req as NextApiRequest)
 

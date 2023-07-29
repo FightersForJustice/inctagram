16ebafea5fc3e932f7d479fd91c970c999850d67
Iryna Rudzko <irinrudko@gmail.com>
2023-07-30 00:27:09 +0200

    refactor: use getSereverSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,40 @@
-import { PageWrapper } from '@/components/PageWrapper/PageWrapper'
 import { NextPageWithLayout } from './_app'
 import { getLayout } from '@/components/Layout/Layout'
-import Login from './auth/login'
-import { useRouter } from 'next/router'
-import { useMeQuery } from '@/assets/api/auth/authQueryApi'
-import { useEffect } from 'react'
-import { Loading } from '@/components/common/Loaders/Loading'
-import { User, setUser } from '@/assets/api/auth/userSlice'
-import { useAppDispatch } from '@/hooks'
+import { GetServerSideProps, NextApiRequest } from 'next'
+import { serverAPI } from '@/assets/api/api'
+import { authRouts } from '@/components/common/Auth/authRoutes'
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  try {
+    const isAuth = await serverAPI.auth.meServer(req as NextApiRequest)
+
+    if (!isAuth) {
+      return {
+        redirect: {
+          destination: authRouts.login,
+          permanent: false,
+        },
+      }
+    }
+
+    return {
+      redirect: {
+        destination: '/home',
+        permanent: false,
+      },
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        destination: authRouts.login,
+        permanent: false,
+      },
+    }
+  }
+}
 
 const Home: NextPageWithLayout = () => {
-  const router = useRouter()
-  const { isError, data } = useMeQuery()
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    if (data) {
-      dispatch(setUser(data as User))
-      router.push('/home')
-    }
-  }, [data])
-
-  if (isError) {
-    return (
-      <PageWrapper>
-        <Login />
-      </PageWrapper>
-    )
-  }
-
-  return (
-    <PageWrapper>
-      <Loading />
-    </PageWrapper>
-  )
+  return <></>
 }
 
 Home.getLayout = getLayout

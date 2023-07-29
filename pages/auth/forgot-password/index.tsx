aa1599d10daac33874355ba902c70474e74567c3
Iryna Rudzko <irinrudko@gmail.com>
2023-07-30 00:00:39 +0200

    feat: hide page when user is authorized

--- pages/auth/forgot-password/index.tsx
+++ pages/auth/forgot-password/index.tsx
@@ -1,12 +1,10 @@
 import { PageWrapper } from 'components/PageWrapper/PageWrapper'
 import ForgotPasswordContainer from '@/components/auth/ForgotPassword/ForgotPasswordContainer'
 import { getLayout } from '@/components/Layout/Layout'
+import { GetServerSideProps } from 'next'
+import { hideWhenAuth } from '@/utils/getServerSideProps/hideWhenAuth'
 
-export const getStaticProps = async () => {
-  return {
-    props: {},
-  }
-}
+export const getServerSideProps: GetServerSideProps = hideWhenAuth
 
 const ForgotPassword = () => {
   return (

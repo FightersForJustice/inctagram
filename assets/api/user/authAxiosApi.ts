a32c96a393f58dcd0b8bc4bf5500b9cc90612e3f
Iryna Rudzko <irinrudko@gmail.com>
2023-07-29 17:22:55 +0200

    refactor: login request

--- assets/api/user/authAxiosApi.ts
+++ assets/api/user/authAxiosApi.ts
@@ -1,20 +1,13 @@
 import { authRouts } from '@/components/common/Auth/authRoutes'
 import { AxiosResponse } from 'axios'
 import { UserData, ServerErrorResponse, LoginParamsData, ServerLoginResponse } from '../auth/authTypes'
-import axiosInstance from '../instance'
+import { instance } from '../instance'
 
 export const authAxiosApi = {
   me() {
-    return axiosInstance.get<AxiosResponse<UserData | ServerErrorResponse>>(authRouts.me).then(({ data }) => data)
+    return instance.get<AxiosResponse<UserData | ServerErrorResponse>>(authRouts.me).then(({ data }) => data)
   },
   login(data: LoginParamsData) {
-    return axiosInstance
-      .post<AxiosResponse<ServerLoginResponse | ServerErrorResponse>>(authRouts.login, data)
-      .then(({ data }) => {
-        const loginResponse = data as unknown as ServerLoginResponse
-        const accessToken = loginResponse.accessToken
-
-        return data
-      })
+    return instance.post<AxiosResponse<ServerLoginResponse | ServerErrorResponse>>(authRouts.login, data).then(({ data }) => data)
   },
 }

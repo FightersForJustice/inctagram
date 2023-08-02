96a8cee631863f3945ced7a4ffa17befe4139e82
Iryna Rudzko <irinrudko@gmail.com>
2023-08-02 13:14:45 +0200

    refactor: remove AxiosReponse type

--- assets/api/user/profileAxiosApi.ts
+++ assets/api/user/profileAxiosApi.ts
@@ -12,10 +12,10 @@ const getProfileFromServer = async (req: NextApiRequest) => {
 
 export const profileAxiosApi = {
   getProfile() {
-    return instance.get<AxiosResponse<UserProfile>>(userRouts.profile).then(({ data }) => data)
+    return instance.get<UserProfile>(userRouts.profile).then(({ data }) => data)
   },
   updateProfile() {
-    return instance.post<AxiosResponse<UpdateUserProfile>>(userRouts.profile).then(({ data }) => data)
+    return instance.post<UpdateUserProfile>(userRouts.profile).then(({ data }) => data)
   },
   getProfileFromServer,
 }

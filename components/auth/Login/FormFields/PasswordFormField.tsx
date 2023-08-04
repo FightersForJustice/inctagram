f565515dac2233948363a055ea06c76c581fe49f
maskpa100 <maskpa@yandex.ru>
2023-08-05 01:47:31 +0800

    refactor

--- components/auth/Login/FormFields/PasswordFormField.tsx
+++ components/auth/Login/FormFields/PasswordFormField.tsx
@@ -8,28 +8,27 @@ import { PasswordFormFieldProps } from '../type'
 
 
 const PasswordFormField: React.FC<PasswordFormFieldProps> = ({ register, trigger, errors, serverError, setServerError }) => {
-  const PasswordFormField: React.FC<PasswordFormFieldProps> = ({ register, trigger, errors, serverError, setServerError }) => {
-    const { t } = useTranslation()
-    const translate = (key: string): string => t(`login_form.${key}`)
-    useEffect(() => {
-      Object.keys(errors).forEach((fieldName: any) => {
-        trigger(fieldName);
-      });
-    }, [t])
-    return (
-      <div className={style.input_container}>
-        <PasswordInput
-          onBlurCapture={() => trigger('password')}
-          className={errors.password ? style.error : ''}
-          validation={{ ...register('password', Validate(ValidateField.Password)) }}
-          placeholder="******************"
-          label={translate('password')}
-          onClick={() => setServerError('')}
-        />
-        {errors.password && <p className={style.errorText}>{errors.password.message}</p>}
-        {serverError && <p className={style.errorText}>{serverError}</p>}
-      </div>
-    )
-  }
+  const { t } = useTranslation()
+  const translate = (key: string): string => t(`login_form.${key}`)
+  useEffect(() => {
+    Object.keys(errors).forEach((fieldName: any) => {
+      trigger(fieldName);
+    });
+  }, [t])
+  return (
+    <div className={style.input_container}>
+      <PasswordInput
+        onBlurCapture={() => trigger('password')}
+        className={errors.password ? style.error : ''}
+        validation={{ ...register('password', Validate(ValidateField.Password)) }}
+        placeholder="******************"
+        label={translate('password')}
+        onClick={() => setServerError('')}
+      />
+      {errors.password && <p className={style.errorText}>{errors.password.message}</p>}
+      {serverError && <p className={style.errorText}>{serverError}</p>}
+    </div>
+  )
+}
 
-  export default PasswordFormField
+export default PasswordFormField

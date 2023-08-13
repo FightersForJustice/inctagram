d9935bfc760e5e59456cb94b3388731b24d3962b
maskpa100 <maskpa@yandex.ru>
2023-08-13 14:26:06 +0800

    refactor

--- assets/api/instance.ts
+++ assets/api/instance.ts
@@ -2,12 +2,13 @@ import axios, { AxiosInstance } from 'axios'
 import { parse } from 'cookie'
 import { NextApiRequest } from 'next'
 import { getAccessTokenFromCookie } from '@/utils/cookies'
+import { baseUrl } from './common.api'
 
 const token = getAccessTokenFromCookie()
 
 //to get client-side requests
 export const instance = axios.create({
-  baseURL: 'https://inctagram.work/api/v1',
+  baseURL: baseUrl,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${token}`,
@@ -20,7 +21,7 @@ export const createAxiosServerInstance = (req: NextApiRequest): AxiosInstance =>
   const accessToken = cookies.accessToken || ''
 
   const serverInstance = axios.create({
-    baseURL: 'https://inctagram.work/api/v1',
+    baseURL: baseUrl,
     withCredentials: true,
     headers: {
       Authorization: `Bearer ${accessToken}`,

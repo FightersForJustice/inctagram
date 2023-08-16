5c137718b4aae83e537a10645357cf1312148ca4
Qurial <konvert955@gmail.com>
2023-08-16 18:27:16 +0300

    refactor: date picker tests

--- @ui/ui-kit/DatePicker/__test__/Datepicker.test.tsx
+++ @ui/ui-kit/DatePicker/__test__/Datepicker.test.tsx
@@ -1,13 +1,39 @@
-import { render, cleanup, screen, fireEvent } from '@testing-library/react'
+import { render, cleanup, screen, fireEvent, waitFor } from '@testing-library/react'
 import { MainDatePicker } from '../DatePicker'
 describe('date picker renders correctly', () => {
-  it('date picker render', () => {
+  afterEach(cleanup)
+
+  it('should render default correctly', () => {
     const { asFragment } = render(<MainDatePicker />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render error correctly', () => {
+    const { asFragment } = render(<MainDatePicker value="99/99/9999" />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render disabled correctly', () => {
+    const { asFragment } = render(<MainDatePicker disabled />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render default with open picker correctly', () => {
+    const { asFragment } = render(<MainDatePicker />)
+
+    fireEvent.click(screen.getByRole('button'))
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should render error with open picker correctly', () => {
+    const { asFragment } = render(<MainDatePicker value="99/99/9999" />)
+
+    fireEvent.click(screen.getByRole('button'))
     expect(asFragment()).toMatchSnapshot()
   })
 })
 
-describe('Date picker', () => {
+describe('Date input', () => {
   afterEach(cleanup)
 
   it('renders correctly', () => {
@@ -78,19 +104,12 @@ describe('Date picker', () => {
     expect(datePickerElement.getAttribute('value')).toBe('01/01/2100')
   })
 
-  it('should open picker when calendar button clicked', () => {
-    render(<MainDatePicker />)
-
-    const datePickerButtonElement = screen.getByRole('button')
-    fireEvent.click(datePickerButtonElement)
-    expect(screen.getByRole('grid')).toBeInTheDocument()
-  })
-
   it('should focus input when it`s clicked', () => {
     render(<MainDatePicker />)
 
     const datePickerElement = screen.getByPlaceholderText('MM/DD/YYYY')
     fireEvent.click(datePickerElement)
+
     expect(datePickerElement).toBeEnabled()
   })
 
@@ -106,72 +125,98 @@ describe('Date picker', () => {
 
     const labelElement = screen.getByText('label')
     fireEvent.click(labelElement)
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY')).toBeEnabled()
   })
 
+  it('should open picker when calendar button clicked', () => {
+    render(<MainDatePicker />)
+
+    const datePickerButtonElement = screen.getByRole('button')
+    fireEvent.click(datePickerButtonElement)
+
+    expect(screen.getByRole('grid')).toBeInTheDocument()
+  })
+})
+
+describe('Date picker', () => {
+  const openPicker = () => {
+    fireEvent.click(screen.getByRole('button'))
+  }
+
+
+  afterEach(cleanup)
   it('should change the date when "number" button clicked', () => {
     render(<MainDatePicker value="10/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByRole('gridcell', { name: '24' }))
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY').getAttribute('value')).toBe('10/24/2000')
   })
 
   it('should change the month when "next month" button clicked', () => {
     render(<MainDatePicker value="10/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByTitle('Next month'))
     fireEvent.click(screen.getAllByRole('gridcell', { name: '20' })[0])
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY').getAttribute('value')).toBe('11/20/2000')
   })
 
   it('should change the month when "previous month" button clicked', () => {
     render(<MainDatePicker value="10/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByTitle('Previous month'))
     fireEvent.click(screen.getAllByRole('gridcell', { name: '20' })[0])
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY').getAttribute('value')).toBe('09/20/2000')
   })
 
   it('should change the year when "next month" button clicked on last month', () => {
     render(<MainDatePicker value="12/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByTitle('Next month'))
     fireEvent.click(screen.getAllByRole('gridcell', { name: '20' })[0])
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY').getAttribute('value')).toBe('01/20/2001')
   })
 
   it('should change the year when "previous month" button clicked on first month', () => {
     render(<MainDatePicker value="01/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByTitle('Previous month'))
     fireEvent.click(screen.getAllByRole('gridcell', { name: '20' })[0])
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY').getAttribute('value')).toBe('12/20/1999')
   })
 
   it('should disable "Previous month" button on first month of first year', () => {
     render(<MainDatePicker value="01/01/1900" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
+
     expect(screen.getByTitle('Previous month')).toBeDisabled()
   })
 
   it('should disable "Next month" button on last month of last year', () => {
     render(<MainDatePicker value="12/31/2099" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
+
     expect(screen.getByTitle('Next month')).toBeDisabled()
   })
 
   it('should open year selector when "year" arrow button clicked', () => {
     render(<MainDatePicker value="01/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByLabelText('calendar view is open, switch to year view'))
+
     expect(screen.getByText('1900')).toBeInTheDocument()
     expect(screen.getByText('2099')).toBeInTheDocument()
   })
@@ -179,25 +224,28 @@ describe('Date picker', () => {
   it('should change year when "year" button clicked', () => {
     render(<MainDatePicker value="10/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     fireEvent.click(screen.getByLabelText('calendar view is open, switch to year view'))
     fireEvent.click(screen.getByText('1950'))
+
     expect(screen.getByPlaceholderText('MM/DD/YYYY').getAttribute('value')).toBe('10/10/1950')
   })
   it('should focus number button', () => {
     render(<MainDatePicker value="10/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     const dateNumberButton = screen.getByRole('gridcell', { name: '24' })
     fireEvent.focus(dateNumberButton)
+
     expect(dateNumberButton).toHaveClass('Mui-focusVisible')
   })
 
   it('should have "selected" class on selected number button', () => {
     render(<MainDatePicker value="10/10/2000" />)
 
-    fireEvent.click(screen.getByRole('button'))
+    openPicker()
     const dateNumberButton = screen.getByRole('gridcell', { name: '10' })
+    
     expect(dateNumberButton).toHaveClass('Mui-selected')
   })
 })

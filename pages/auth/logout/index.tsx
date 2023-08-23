ccff1c27f33336c67ba933a7f910be316080ca42
maskpa100 <maskpa@yandex.ru>
2023-08-24 00:15:58 +0800

    fead : logout

--- pages/auth/logout/index.tsx
+++ pages/auth/logout/index.tsx
@@ -1,13 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { getLayout } from '@/components/Layout/Layout'
 import { useLogoutMutation } from '@/assets/api/auth/authQueryApi'
 import { authRouts } from '@/components/common/Auth/authRoutes'
 import { removeAccessTokenCookie } from '@/utils/cookies'
+import { GetServerSideProps, NextApiRequest } from 'next'
+import { axiosAPI } from '@/assets/api/api'
+import Modal from '@/@ui/ui-kit/Modal/Modal'
+import s from './index.module.scss'
+import { Button } from '@/@ui/ui-kit/Button/Button'
+import { useTranslation } from 'react-i18next'
+import { BUTTON_COLORS } from '@/@ui/ui-kit/Button/constants'
 
-const Logout = () => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const isAuth = await axiosAPI.auth.meServer(req as NextApiRequest)
+  return {
+    props: {
+      isAuth,
+    },
+  }
+}
+
+const Logout = (props) => {
+  const { t } = useTranslation()
+  const translate = (key: string): string => t(`logout.${key}`)
   const [logout] = useLogoutMutation()
   const router = useRouter()
+  const [ModalActive, setModalActive] = useState(true)
+
+  const handlerYes = ()=>{}
+  const handlerNo = ()=>{}
+  /*
   useEffect(() => {
     const performLogout = async () => {
       try {
@@ -23,6 +46,20 @@ const Logout = () => {
     }
     performLogout()
   }, [logout])
+  */
+  return (
+    <>
+      <Modal title={translate('title')} active={ModalActive} setActive={setModalActive} close={true}>
+        <div className={s.content}>
+          <p>{translate('text')} “{props.isAuth.email}”?</p>
+          <div className={s.buttons}>
+            <Button color={BUTTON_COLORS.OUTLINED} text={translate('yes')} onClick={handlerYes}></Button>
+            <Button text={translate('no')} onClick={handlerNo}></Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  )
 }
 Logout.getLayout = getLayout
 export default Logout

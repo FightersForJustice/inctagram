c671d2bda9e413a8070c1b8bc7cc807f51898993
TURP1 <turpicrypto@gmail.com>
2023-08-28 14:51:55 +0300

    format

--- @ui/ui-kit/Textareas/Textarea.tsx
+++ @ui/ui-kit/Textareas/Textarea.tsx
@@ -13,7 +13,7 @@ export type TextAreaProps = {
   placeholder?: string
   label?: string
   color?: (typeof TEXTAEREA_COLORS)[keyof typeof TEXTAEREA_COLORS]
-} & React.TextareaHTMLAttributes<HTMLTextAreaElement> 
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
 export const TextArea: React.FC<TextAreaProps> = ({
   value,
@@ -25,7 +25,6 @@ export const TextArea: React.FC<TextAreaProps> = ({
   errorMessage = 'Error text',
   placeholder = 'Textarea',
   color = TEXTAEREA_COLORS.DEFAULT,
-
 }) => {
   const textareaClasses = classNames(styles.textarea, {
     [styles.Default]: color === TEXTAEREA_COLORS.DEFAULT,
@@ -36,7 +35,7 @@ export const TextArea: React.FC<TextAreaProps> = ({
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     if (!disabled) {
       if (onChange) {
-        onChange(event);
+        onChange(event)
       }
     }
   }
@@ -44,13 +43,7 @@ export const TextArea: React.FC<TextAreaProps> = ({
   return (
     <div>
       <p className={styles.title}>{label}</p>
-      <textarea
-        className={textareaClasses}
-        value={value}
-        disabled={disabled}
-        placeholder={placeholder}
-        onChange={handleChange}
-      />
+      <textarea className={textareaClasses} value={value} disabled={disabled} placeholder={placeholder} onChange={handleChange} />
       {hasError && <p className={styles.error_message}>{errorMessage}</p>}
     </div>
   )

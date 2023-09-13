c9fbed9352f3e0d60b7014780187adc8091d9283
maskpa100 <maskpa@yandex.ru>
2023-09-13 14:51:56 +0800

    refactor

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -7,6 +7,8 @@ import { withAuth } from '@/utils/withAuth'
 import { GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
 import { Example } from './slider/Slider'
+import s from './home.module.scss'
+import Image from 'next/image'
 
 export const getServerSideProps: GetServerSideProps = withAuth(async () => {
   return { props: {} }
@@ -26,16 +28,13 @@ const Home = (props: HomeType) => {
 
   return (
     <PageWrapper>
-      <button
-        onClick={handleClick}
-        style={{
-          width: '200px',
-          height: '50px',
-        }}
-      >
-        Profile Settings
-      </button>
-      <Example items={['post/5.png', 'post/1.jpg', 'post/2.jpg', 'post/3.jpg', 'post/4.jpg']} />
+      <div className={s.content}>
+        <div className={s.user}>
+          <Image className={s.avatar} width={36} height={36} src="/post/5.png" alt="Avatar" />
+          <span className={s.name}>URLProfiele</span>
+        </div>
+        <Example items={['post/5.png', 'post/1.jpg', 'post/2.jpg', 'post/3.jpg', 'post/4.jpg']} />
+      </div>
     </PageWrapper>
   )
 }

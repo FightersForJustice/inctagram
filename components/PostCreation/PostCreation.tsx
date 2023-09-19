ff98bfc22320ba120e0c5c2b1b4516eed5764ea1
Qurial <konvert955@gmail.com>
2023-09-19 21:31:25 +0300

    feat: implement logic of deleting all photos on return to 'Add Photo' module

--- components/PostCreation/PostCreation.tsx
+++ components/PostCreation/PostCreation.tsx
@@ -1,17 +1,26 @@
 import Modal from '@/@ui/ui-kit/Modal/Modal'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CreatePostPaginator from './CreatePostPaginator'
 import AddPhoto from './AddPhoto/AddPhoto'
 import Cropping from './Cropping/Cropping'
 import Filters from './Filters/Filters'
 import Uploading from './Uploading/Uploading'
 import style from './PostCreation.module.scss'
+import { useDispatch } from 'react-redux'
+import { clearPhotos } from '@/core/slices/postCreationSlice'
 
 const PostCreation = () => {
+  const dispatch = useDispatch()
   const [isModalOpen, setIsModalOpen] = useState(true)
   const [moduleNum, setModuleNum] = useState(0)
   const moduleNames = ['Add Photo', 'Cropping', 'Filters', 'Publication']
   const modules = [<AddPhoto setModuleNum={setModuleNum} />, <Cropping />, <Filters />, <Uploading />]
+
+  useEffect(() => {
+    if(moduleNum !== 0) return 
+    dispatch(clearPhotos())
+  }, [moduleNum])
+
   return (
     <>
       {isModalOpen && (

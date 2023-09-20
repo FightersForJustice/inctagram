2b2cd61c9b49451bb2a1a50199ed98098ae2ad13
Qurial <konvert955@gmail.com>
2023-09-20 16:52:46 +0300

    feat: add "upload more photos button"

--- components/PostCreation/Cropping/Cropping.tsx
+++ components/PostCreation/Cropping/Cropping.tsx
@@ -1,14 +1,28 @@
 import { usePostCreationDataSelector } from '@/core/selectors/postCreationSelector'
 import { Example } from '@/@ui/ui-kit/Slider/Slider'
+import style from './Cropping.module.scss'
+import Image from 'next/image'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 const Cropping = () => {
   const { photos } = usePostCreationDataSelector()
+  const [isAddPhotosClosed, setIsAddPhotosClosed] = useState(true)
   const photosLinks = photos.map((photoObj: any) => {
     return photoObj.photo
   })
   return (
     <div>
       <Example items={photosLinks} />
+      <button className={style.button} onClick={() => setIsAddPhotosClosed((prev) => !prev)}>
+        <Image src="../icons/image-outline.svg" alt="" width={32} height={32} />
+      </button>
+
+      <div className={classNames(style.addPhotosContainer, { [style.addPhotosContainerHidden]: isAddPhotosClosed })}>
+        {photosLinks.map((photo: string) => (
+          <Image src={photo} alt="" width={86} height={86} />
+        ))}
+      </div>
     </div>
   )
 }

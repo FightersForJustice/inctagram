64ea6fc38628802b56887ce6b14f0f49fc05e013
Qurial <konvert955@gmail.com>
2023-09-20 16:05:27 +0300

    feat: implement carousel of photos

--- components/PostCreation/Cropping/Cropping.tsx
+++ components/PostCreation/Cropping/Cropping.tsx
@@ -1,5 +1,16 @@
+import { usePostCreationDataSelector } from '@/core/selectors/postCreationSelector'
+import { Example } from '@/@ui/ui-kit/Slider/Slider'
+
 const Cropping = () => {
-  return <div>Cropping</div>
+  const { photos } = usePostCreationDataSelector()
+  const photosLinks = photos.map((photoObj: any) => {
+    return photoObj.photo
+  })
+  return (
+    <div>
+      <Example items={photosLinks} />
+    </div>
+  )
 }
 
 export default Cropping

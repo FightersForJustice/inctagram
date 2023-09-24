cb69528867ed489c1e73a3373d388f330ecd6377
maskpa100 <maskpa@yandex.ru>
2023-09-24 19:59:54 +0800

    refactor

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,5 +1,4 @@
 import { axiosAPI } from '@/assets/api/api'
-import { UserData } from '@/assets/api/auth/authTypes'
 import { getSideBarLayout } from '@/components/Layout/SideBarLayout/SideBarLayout'
 import { PageWrapper } from '@/components/common/PageWrapper/PageWrapper'
 import { GetServerSideProps, NextApiRequest } from 'next'
@@ -7,11 +6,9 @@ import s from './style.module.scss'
 import Image from 'next/image'
 import { Button } from '@/@ui/ui-kit/Button/Button'
 import { BUTTON_COLORS } from '@/@ui/ui-kit/Button/constants'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { userRouts } from '@/app/routes/userRouts'
 
-import avatar from '../../public/img/avatar.jpg'
 import img1 from '../../public/img/post/img1.jpg'
 import img2 from '../../public/img/post/img2.jpg'
 import img3 from '../../public/img/post/img3.jpg'
@@ -21,34 +18,63 @@ import img6 from '../../public/img/post/img6.jpg'
 import img7 from '../../public/img/post/img7.jpg'
 import img8 from '../../public/img/post/img8.jpg'
 import { withAuth } from '@/utils/getServerSideProps/withAuth'
+import { useEffect, useState } from 'react'
+import { ProfileType } from '@/components/Profile-Settings/profileSettingsTypes'
 
 export const getServerSideProps: GetServerSideProps = withAuth(async ({ req }) => {
-  const isAuth = await axiosAPI.auth.meServer(req as NextApiRequest)
+  const userProfile = await axiosAPI.profile.getProfileFromServer(req as NextApiRequest)
+
+  if (!userProfile) {
+    return {
+      notFound: true,
+    }
+  }
+
   return {
     props: {
-      isAuth,
+      userProfile,
     },
   }
 })
 
-type HomeType = {
-  isAuth: UserData
-}
+const MyProfile = ({ userProfile }: ProfileType) => {
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8]
 
-const MyProfile = (props: HomeType) => {
+  const [imagesContent, setImagesContent] = useState(images)
+  const [fetching, setFetching] = useState(false)
+
   const router = useRouter()
+
+  useEffect(() => {
+    if (fetching) {
+      setImagesContent((prevArray) => [...prevArray, ...images])
+      setFetching(false)
+    }
+  }, [fetching])
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll)
+    return function () {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   const handleClick = () => {
     router.push(userRouts.profileSettings)
   }
-
+  const handleScroll = (e: Event) => {
+    const target = e.target as Document
+    if (target.documentElement.scrollHeight - (target.documentElement.scrollTop + window.innerHeight) < 100) {
+      setFetching(true)
+    }
+  }
   return (
     <PageWrapper>
       <div className={s.main}>
         <div className={s.header}>
-          <Image className={s.avatar} width={204} height={204} src={avatar} alt="Avatar" />
+          <Image className={s.avatar} width={204} height={204} src={userProfile.avatars[0].url} alt="Avatar" />
           <div className={s.info}>
             <div className={s.blockTop}>
-              <div className={s.name}>URLProfiele</div>
+              <div className={s.name}>{userProfile.firstName}</div>
               <div className={s.settings}>
                 <Button color={BUTTON_COLORS.BASIC} onClick={handleClick} text="Profile Settings"></Button>
               </div>
@@ -68,31 +94,14 @@ const MyProfile = (props: HomeType) => {
               </div>
             </div>
             <div className={s.blockBottom}>
-              <p className={s.text}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco{' '}
-                <Link href="#" className={s.link}>
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </Link>
-              </p>
+              <p className={s.text}>{userProfile.aboutMe}</p>
             </div>
           </div>
         </div>
         <div className={s.post}>
-          <Image width={234} height={228} src={img1} alt="Post" />
-          <Image width={234} height={228} src={img2} alt="Post" />
-          <Image width={234} height={228} src={img3} alt="Post" />
-          <Image width={234} height={228} src={img4} alt="Post" />
-          <Image width={234} height={228} src={img5} alt="Post" />
-          <Image width={234} height={228} src={img6} alt="Post" />
-          <Image width={234} height={228} src={img7} alt="Post" />
-          <Image width={234} height={228} src={img8} alt="Post" />
-          <Image width={234} height={228} src={img3} alt="Post" />
-          <Image width={234} height={228} src={img4} alt="Post" />
-          <Image width={234} height={228} src={img2} alt="Post" />
-          <Image width={234} height={228} src={img3} alt="Post" />
-          <Image width={234} height={228} src={img4} alt="Post" />
-          <Image width={234} height={228} src={img5} alt="Post" />
+          {imagesContent.map((img, index) => (
+            <Image width={234} height={228} src={img} alt="Post" key={index} />
+          ))}
         </div>
       </div>
     </PageWrapper>

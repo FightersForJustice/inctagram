6fc66d258d8f0015d966dc310e9530536529169f
maskpa100 <maskpa@yandex.ru>
2023-10-06 22:22:38 +0800

    refactor

--- pages/profile/profile-settings/index.tsx
+++ pages/profile/profile-settings/index.tsx
@@ -1,9 +1,9 @@
 import { axiosAPI } from '@/assets/api/api'
-import { getLayout } from '@/components/Layout/Layout'
 import { withAuth } from '@/utils/getServerSideProps/withAuth'
 import { GetServerSideProps, NextApiRequest } from 'next'
 import { ProfileType } from '@/components/Profile-Settings/profileSettingsTypes'
 import ProfileSettings from '@/components/Profile-Settings/ProfileSettings'
+import { getSideBarLayout } from '@/components/Layout/SideBarLayout/SideBarLayout'
 
 export const getServerSideProps: GetServerSideProps = withAuth(async ({ req }) => {
   const isAuth = await axiosAPI.auth.meServer(req as NextApiRequest)
@@ -26,5 +26,5 @@ const ProfileSettingsPage = ({ userProfile }: ProfileType) => {
   return <ProfileSettings userProfile={userProfile} />
 }
 
-ProfileSettingsPage.getLayout = getLayout
+ProfileSettingsPage.getLayout = getSideBarLayout
 export default ProfileSettingsPage

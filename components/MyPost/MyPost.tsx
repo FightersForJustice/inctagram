f0ebe9bf2ef0e0af6df3dab26395d1b2fabbddfe
maskpa100 <maskpa@yandex.ru>
2023-10-13 11:59:25 +0800

    refactor

--- components/MyPost/MyPost.tsx
+++ components/MyPost/MyPost.tsx
@@ -2,6 +2,8 @@ import classNames from 'classnames'
 import s from './MyPost.module.scss'
 import MyCarousel from '@/@ui/ui-kit/Carousel'
 import Image from 'next/image'
+import { Button } from '@/@ui/ui-kit/Button/Button'
+import { BUTTON_COLORS } from '@/@ui/ui-kit/Button/constants'
 
 type ModalType = {
   active: boolean
@@ -45,13 +47,74 @@ const MyPost: React.FC<ModalType> = ({ active, setMyPostActive }) => {
             />
           </div>
           <div className={s.post}>
-            <div className={s.infoPost}>
-              <div className={s.user}>
-                <Image width={36} height={36} src="/img/post/img7.jpg" alt="Avatar"></Image>
-                <div className={s.name}>URLProfiele</div>
+            <div className={s.postBlock}>
+              <div className={s.infoPost}>
+                <div className={s.user}>
+                  <Image width={36} height={36} src="/img/post/img7.jpg" alt="Avatar"></Image>
+                  <div className={s.name}>URLProfiele</div>
+                </div>
+                <div className={s.moreHorizontal}>
+                  <Image width={24} height={24} src="/sidebar-icons/more-horizontal.svg" alt="More horizontal"></Image>
+                </div>
               </div>
-              <div className={s.moreHorizontal}>
-                <Image width={24} height={24} src="/sidebar-icons/more-horizontal.svg" alt="More horizontal"></Image>
+              <div className={s.comments}>
+                <div className={s.comment}>
+                  <div className={s.avatar}>
+                    <Image width={36} height={36} src="/img/post/img7.jpg" alt="Avatart"></Image>
+                  </div>
+                  <div className={s.text}>
+                    <p>
+                      <span className={s.name}>URLProfiele</span>
+                      <span className={s.textComment}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua.
+                      </span>
+                    </p>
+                    <p className={s.time}>2 hours ago</p>
+                  </div>
+                </div>
+                <div className={s.comment}>
+                  <div className={s.avatar}>
+                    <Image width={36} height={36} src="/img/post/img3.jpg" alt="Avatart"></Image>
+                  </div>
+                  <div className={s.text}>
+                    <p>
+                      <span className={s.name}>URLProfiele</span>
+                      <span className={s.textComment}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua.
+                      </span>
+                    </p>
+                    <p className={s.time}>
+                      6 hours ago<span className={s.answer}>Answer</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className={s.footer}>
+              <div className={s.icons}>
+                <div className={s.left}>
+                  <Image width={24} height={24} src="/sidebar-icons/heart-outline.svg" alt="Heart outline"></Image>
+                  <Image width={24} height={24} src="/sidebar-icons/paper-plane-outline.svg" alt="Paper plane outline"></Image>
+                </div>
+                <Image width={24} height={24} src="/sidebar-icons/bookmark-outline.svg" alt="Bookmark outline"></Image>
+              </div>
+              <div className={s.likes}>
+                <div className={s.avatars}>
+                  <Image width={24} height={24} src="/img/post/img7.jpg" alt="Avatar"></Image>
+                  <Image width={24} height={24} src="/img/post/img5.jpg" alt="Avatar"></Image>
+                  <Image width={24} height={24} src="/img/post/img4.jpg" alt="Avatar"></Image>
+                </div>
+                <span className={s.like}>
+                  2 243 <b>"Like"</b>
+                </span>
+              </div>
+              <p className={s.time}>July 3, 2021</p>
+              <div className={s.addComment}>
+                <input type="text" placeholder="Add a Comment..." />
+                <Button color={BUTTON_COLORS.GHOST} text="Publish"></Button>
               </div>
             </div>
           </div>

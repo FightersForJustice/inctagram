c58fffd20fcb73974fad31646baf6c2be6e0cf13
Qurial <konvert955@gmail.com>
2023-10-15 17:48:30 +0300

    fix: position of adding photos on cropping page

--- components/PostCreation/Cropping/Cropping.tsx
+++ components/PostCreation/Cropping/Cropping.tsx
@@ -7,33 +7,36 @@ import { setPhoto } from '@/core/slices/postCreationSlice'
 import { useDispatch } from 'react-redux'
 import MyCarousel from '@/@ui/ui-kit/Carousel'
 import { handlerImageUpload } from '../handlerImageUpload'
-
 const Cropping = () => {
   const dispatch = useDispatch()
   const { photos } = usePostCreationDataSelector()
   const [isAddPhotosClosed, setIsAddPhotosClosed] = useState(true)
   const [image, setImage] = useState('')
-
   useEffect(() => {
     if (!image) return
     dispatch(setPhoto({ photo: image }))
   }, [image])
-
   const photosLinks = photos.map((photoObj: any) => {
     return photoObj.photo
   })
-
   return (
     <div>
       <MyCarousel items={photosLinks} />
-      <button className={style.button} onClick={() => setIsAddPhotosClosed((prev) => !prev)}>
-        <Image src="../icons/image-outline.svg" alt="" width={32} height={32} />
+      <button
+        className={classNames(style.button, { [style.buttonFocused]: !isAddPhotosClosed })}
+        onClick={() => setIsAddPhotosClosed((prev) => !prev)}
+      >
+        {isAddPhotosClosed ? (
+          <Image src="../icons/image-outline.svg" alt="" width={24} height={24} />
+        ) : (
+          <Image src="../icons/image.svg" alt="" width={24} height={24} />
+        )}
       </button>
 
       <div className={classNames(style.addPhotosContainer, { [style.addPhotosContainerHidden]: isAddPhotosClosed })}>
         <div className={style.photosContainer}>
           {photosLinks.map((photo: string) => (
-            <Image src={photo} alt="" width={86} height={86} className={style.uploadedPhoto} />
+            <Image src={photo} alt="" width={80} height={80} className={style.uploadedPhoto} />
           ))}
         </div>
         <div className={style.addPhotoButtonContainer}>
@@ -46,12 +49,11 @@ const Cropping = () => {
             disabled={photos.length === 10}
           />
           <label htmlFor="upload-button" className={style.uploadButton}>
-            <Image src="../sidebar-icons/plus-circle-outline.svg" alt="" width={24} height={24} />
+            <Image src="../sidebar-icons/plus-circle-outline.svg" alt="" width={36} height={36} />
           </label>
         </div>
       </div>
     </div>
   )
 }
-
 export default Cropping

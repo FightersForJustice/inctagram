cb44f85fc7d5653aecc2e86d71928b060388b540
Qurial <konvert955@gmail.com>
2023-10-15 14:45:30 +0300

    refactor: removed the unnecessary step of uploading photos

--- components/PostCreation/AddPhoto/AddPhoto.tsx
+++ components/PostCreation/AddPhoto/AddPhoto.tsx
@@ -1,6 +1,5 @@
-import { ChangeEvent, Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import style from './AddPhoto.module.scss'
-import { Button } from '@/@ui/ui-kit/Button/Button'
 import buttonStyle from '@/@ui/ui-kit/Button/Button.module.scss'
 import { useTranslation } from 'react-i18next'
 import classNames from 'classnames'
@@ -18,45 +17,30 @@ const AddPhoto: FC<AddPhotoType> = ({ setModuleNum }) => {
   const [image, setImage] = useState<string>('')
   const { t } = useTranslation()
   useEffect(() => {
-    if(!image) return
+    if (!image) return
     dispatch(setPhoto({ photo: image }))
+    setModuleNum(1)
   }, [image])
 
   return (
     <div className={style.container}>
       <div className={style.addPhotoContainer}>
-        {image ? (
-          <Image src={image} alt="" className={style.image} width={222} height={228} />
-        ) : (
-          <Image width={48} height={48} src="/../icons/image-outline.svg" alt="img" />
-        )}
+        <Image width={48} height={48} src="/../icons/image-outline.svg" alt="img" />
       </div>
-      {!image ? (
-        <>
-          <label
-            htmlFor="custom-upload"
-            className={classNames(buttonStyle.button, buttonStyle.buttonAutoHeight)}
-            style={{ width: '222px', marginTop: '60px' }}
-          >
-            {t('add_profile_photo.select_from_computer')}
-          </label>
-
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => handlerImageUpload(e, setImage)}
-            id="custom-upload"
-            style={{ display: 'none' }}
-          />
-        </>
-      ) : (
-        <Button
-          onClick={() => setModuleNum(1)}
-          text={t('add_profile_photo.add_profile_photo')}
-          style={{ width: '222px', marginTop: '60px' }}
-          variation="AutoHeight"
-        />
-      )}
+      <label
+        htmlFor="custom-upload"
+        className={classNames(buttonStyle.button, buttonStyle.buttonAutoHeight)}
+        style={{ width: '222px', marginTop: '60px' }}
+      >
+        {t('add_profile_photo.select_from_computer')}
+      </label>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => handlerImageUpload(e, setImage)}
+        id="custom-upload"
+        style={{ display: 'none' }}
+      />
     </div>
   )
 }
